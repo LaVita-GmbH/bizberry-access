753e4228ec965ecc3f0d6ff1bfe97fe4cd3f5222
Manuel Stingl <manuel.stingl@lavita.de>
2020-11-16 14:05:43 +0100

    Add issuer to JWTToken util

--- pegasus/utils.py
+++ pegasus/utils.py
@@ -4,6 +4,7 @@ from olympus.security.jwt import JWTToken as BaseJWTToken
 
 class JWTToken(BaseJWTToken):
     def __init__(self, **kwargs):
+        kwargs.setdefault('issuer', settings.JWT_ISSUER)
         kwargs.setdefault('key', settings.JWT_PUBLIC_KEY)
         kwargs.setdefault('algorithm', 'ES512')
         super().__init__(**kwargs)
