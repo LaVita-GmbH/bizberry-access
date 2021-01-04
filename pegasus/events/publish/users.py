798cfa99a676de85d64f0afb659602afffd56b28
Manuel Stingl <manuel.stingl@lavita.de>
2021-01-04 07:47:43 +0100

    Send events with DataChangeEvent

--- pegasus/events/publish/users.py
+++ pegasus/events/publish/users.py
@@ -3,6 +3,7 @@ from kombu import Exchange
 from asgiref.sync import async_to_sync
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from olympus.schemas import DataChangeEvent
 from ... import models
 from ...schemas import response
 from ..base import connection
@@ -21,17 +22,28 @@ users.declare()
 @receiver(post_save, sender=models.User)
 def post_save_user(sender, instance: models.User, created: bool, **kwargs):
     action = 'create' if created else 'update'
-    body = async_to_sync(response.User.from_orm)(instance, tenant=None).json()
+    data = async_to_sync(response.User.from_orm)(instance, tenant=None)
+    body = DataChangeEvent(
+        data=data,
+        data_type='access.user',
+        data_op=getattr(DataChangeEvent.DataOperation, action.upper()),
+    )
     connection.ensure(users, users.publish)(
-        message=body,
+        message=body.json(),
         routing_key=f'v1.data.{action}',
     )
 
 
 @receiver(post_delete, sender=models.User)
 def post_delete_user(sender, instance: models.User, **kwargs):
-    body = json.dumps({'id': instance.id})
+    body = DataChangeEvent(
+        data={
+            'id': instance.id,
+        },
+        data_type='access.user',
+        data_op=DataChangeEvent.DataOperation.DELETE,
+    )
     connection.ensure(users, users.publish)(
-        message=body,
+        message=body.json(),
         routing_key='v1.data.delete',
     )
