a78e739489510eab8536f4062f1fe05a9df8c7d9
Manuel Stingl <manuel.stingl@lavita.de>
2022-09-12 08:59:37 +0200

    run kafka subscribe using management command

--- bb_access/events/subscribe/__init__.py
+++ bb_access/events/subscribe/__init__.py
@@ -1,15 +1,2 @@
-import os
-import django
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bb_access.settings')
-os.environ.setdefault('EVENT_CONSUMER_APP_CONFIG', 'django.conf.settings')
-
-django.setup(set_prefix=False)
-
-
 from ._consumer import consumer
 from . import odoo
-
-
-consumer.run()
---
+++ bb_access/management/commands/kafka_subscribe.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandParser
+from ...events.subscribe import consumer
+
+
+class Command(BaseCommand):
+    help = 'Sync scopes and roles in database with defined scopes and roles in yml file'
+
+    def add_arguments(self, parser: CommandParser):
+        pass
+
+    def handle(self, *args, **options):
+        consumer.run()
