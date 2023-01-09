db16da99816e6c7a630f29d125c87d27a5d7bf92
Manuel Stingl <manuel.stingl@lavita.de>
2023-01-09 14:46:35 +0100

    add setting APP_VERSION

--- bb_access/settings.py
+++ bb_access/settings.py
@@ -24,6 +24,7 @@ load_dotenv()
 
 
 APP_NAME = 'BB Access'
+APP_VERSION = '0.0.1'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
